628c158adf47c83a06cf31e510dca13e58127bdd
linweili <295287765@qq.com>
2018-08-24 18:25:33 +0800

    Initial commit

---
+++ basic/boolean.py
@@ -0,0 +1,22 @@
+#/usr/bin/env python3
+#coding=utf-8
+
+print(True)
+print(False)
+if True:
+    print(True)
+if False:
+    print(False)
+if True and True:
+    print(True)
+if True and False:
+    print(False)
+if True or False:
+    print('ok')
+if not True:
+    print('NOT TURE')
+if not False:
+    print('NOT False')
+age = 18
+if age >= 10:
+    print('age:' ,age)
