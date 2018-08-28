800429b6521760a9e79a38ea863415ec779d5c49
linweili <295287765@qq.com>
2018-08-28 19:38:35 +0800

    functions

--- basic/functions.py
+++ basic/functions.py
@@ -24,10 +24,56 @@ def func(x):
 func(9)
 
 #对参数类型做检查，只允许整数和浮点数类型的参数。数据类型检查可以用内置函数isinstance()实现
+def validate(x):
+    if not isinstance(x,(int,float)):
+        print('类型不正确')
+    else:
+        print('x: %s' % x )
 
+validate(['fds','fsd'])
 
+import math
+def move(x, y, step, angle=0):
+    nx = x + step * math.cos(angle)
+    ny = y - step * math.sin(angle)
+    return nx, ny
+x, y = move(100, 100, 60, math.pi / 6)
+print(x, y)
 
+def power(x, n):
+    s = 1
+    while n > 0:
+        n = n - 1
+        s = s * x
+    return s
 
+print(power(2,10))
+#默认参数
+#必选参数在前，默认参数在后，否则Python的解释器会报错
+def pow(x, n=10):
+    s = 1
+    while n > 0:
+        n = n - 1
+        s = s * x
+    return s
+print(pow(2))
+
+def enroll(name, gender, age=6, city='Beijing'):
+    print('name:', name)
+    print('gender:', gender)
+    print('age:', age)
+    print('city:', city)
+enroll('Sarah', 'F')
+enroll('Adam', 'M', city='Tianjin')
+#定义默认参数要牢记一点：默认参数必须指向不变对象
+def add_end(L=None):
+    if L is None:
+        L = []
+    L.append('END')
+    return L
+
+print( add_end())
+print( add_end())
 
 
 
