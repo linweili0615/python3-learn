f3e5e5afc6b343447af18d939f393a3c8d7a4c16
linweili <295287765@qq.com>
2018-09-05 13:16:53 +0800

    切片去掉两边空格

--- basic/slices.py
+++ basic/slices.py
@@ -29,3 +29,15 @@ def reverse1():
 
 
 #tuple、str 同上
+
+#利用切片操作，实现一个trim()函数，去除字符串首尾的空格，注意不要调用str的strip()方法
+def trim(s):
+    if s[:1] != ' ' and s[-1:] != ' ':
+        return s
+    elif ' ' == s[:1]:
+        return trim(s[1:])
+    else:
+        return trim(s[:-1])
+
+print(trim('fdg'))
+
