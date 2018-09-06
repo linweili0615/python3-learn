16166985e0d638c6682b484c311703cd9ac1e03e
linweili <295287765@qq.com>
2018-09-06 16:29:38 +0800

    try catch

---
+++ basic/loggings.py
@@ -0,0 +1,53 @@
+#/usr/bin/env python3
+#coding=utf-8
+
+#不需要在每个可能出错的地方去捕获错误，只要在合适的层次去捕获错误就可以了。这样一来，就大大减少了写try...except...finally的麻烦
+#记录错误
+import logging
+def foo(s):
+    return 10 / int(s)
+
+def bar(s):
+    return foo(s) * 2
+
+def main():
+    try:
+        bar('0')
+    except Exception as e:
+        logging.exception(e)
+
+main()
+print('END')
+
+#同样是出错，但程序打印完错误信息后会继续执行，并正常退出
+#抛出错误
+class FooError(ValueError):
+    pass
+
+def foo(s):
+    n = int(s)
+    if n==0:
+        raise FooError('invalid value: %s' % s)
+    return 10 / n
+
+foo('0')
+#捕获错误目的只是记录一下，便于后续追踪。但是，由于当前函数不知道应该怎么处理该错误，所以，最恰当的方式是继续往上抛，让顶层调用者去处理
+def foo(s):
+    n = int(s)
+    if n==0:
+        raise ValueError('invalid value: %s' % s)
+    return 10 / n
+
+def bar():
+    try:
+        foo('0')
+    except ValueError as e:
+        print('ValueError!')
+        raise
+
+bar()
+
+# try:
+#     10 / 0
+# except ZeroDivisionError:
+#     raise ValueError('input error!')
