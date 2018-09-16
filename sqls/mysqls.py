5aa63ac499257d4831ad8b92c16ba9f1d93512cd
linweili <295287765@qq.com>
2018-09-16 22:14:37 +0800

    test

--- sqls/mysqls.py
+++ sqls/mysqls.py
@@ -9,7 +9,7 @@ cursor.execute('create table user (id varchar(20) primary key, name varchar(20))
 cursor.execute('insert into user (id, name) values (%s, %s)', ['1', 'Michael'])
 cursor.rowcount
 
-# 提交事务:
+# 提交事务1:
 conn.commit()
 cursor.close()
 # 运行查询:
