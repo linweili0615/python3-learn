4f4b069214ed14b02c1f845b0e36ae5c1951c3e9
linweili <295287765@qq.com>
2018-09-19 13:05:36 +0800

    locust login实例

--- locust_file/draw_locust.py
+++ locust_file/draw_locust.py
@@ -3,17 +3,17 @@
 from locust import TaskSet, HttpLocust, task
 from locust.clients import HttpSession
 
-telno = '15866660001'
+web = 'https://at.tuandai.com'
 
 class DrawTask(TaskSet):
     session = None
-    host = 'https://passport.tuandai.com'
 
     def on_start(self):
+        host = 'https://passport.tuandai.com'
         # 设置sesssion
         self.session = HttpSession(self.host)
-        # 使用 session 来登录
-        # params = 'un={}&pd=123456a&loadType=2&encrypt=0&t=65446446&geetestChallenge=&geetestValidate=&geetestSeccode='.format(telno)
+        res = self.session.get(url=host + '/2login?ret=http://at.tuandai.com/userActivity/midAutumn/we/index')
+        print(res.cookies.values())
         params = {
             'un': '15866660001',
             'pd': '123456a',
@@ -22,20 +22,35 @@ class DrawTask(TaskSet):
             't' : 65446446
         }
         url = '/2login'
-        response = self.session.post(url=self.host + url, data=params)
+        # 使用 session 来登录
+        response = self.session.post(url=host + url, data=params)
         print("login_result:", response.status_code, response.text)
 
     def on_stop(self):
         pass
 
     @task(1)
-    def draw_redpacket(self):
-        response = self.session.post(url='https://at.tuandai.com/userActivity/midAutumn/toFuDaiDraw', data='type=1')
-        print("Draw_result:", response.status_code, response.text)
+    def FIRST_ROUND(self):
+        response = self.session.get(url=web + '/huodong/answerQuestion/drawPrize',
+                                    params='drawFlag=ANSWER_QUESTION_FIRST_ROUND')
+        print("FIRST_ROUND:", response.status_code, response.text)
+
+    @task(1)
+    def SECOND_ROUND(self):
+        response = self.session.get(url=web + '/huodong/answerQuestion/drawPrize',
+                                    params ='drawFlag=ANSWER_QUESTION_SECOND_ROUND')
+        print("SECOND_ROUND:", response.status_code, response.text)
+
+    @task(1)
+    def THIRD_ROUND(self):
+        response = self.session.get(url=web + '/huodong/answerQuestion/drawPrize',
+                                    params='drawFlag=ANSWER_QUESTION_THIRD_ROUND')
+        print("THIRD_ROUND:", response.status_code, response.text)
+
 
 class DrawLocust(HttpLocust):
     task_set = DrawTask
-    host = "https://at.tuandai.com"  # （待测试的ip或者域名）
+    host = web  # （待测试的ip或者域名）
     # 每个模拟用户将在请求之间等待5至15秒
     min_wait = 3000
     max_wait = 6000
